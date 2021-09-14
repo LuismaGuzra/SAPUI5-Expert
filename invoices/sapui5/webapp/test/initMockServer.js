71fb15e5d1b3412265a75a087a566c9d684a1bb5
LuismaGuzra <luisragus11@gmail.com>
2021-09-14 15:07:36 +0000

    odata y mock server

---
+++ invoices/sapui5/webapp/test/initMockServer.js
@@ -0,0 +1,17 @@
+//@ts-nocheck
+sap.ui.define([
+    "../localService/mockserver",
+    "sap/m/MessageBox"
+], function (mockserver, MessageBox) {
+        'use strict';
+
+        var aMockserver = [];
+
+        aMockserver.push(mockserver.init());
+
+        Promise.all(aMockserver).catch(function (oError) {
+            MessageBox.error(oError.message);
+        }).finally(function () {
+            sap.ui.require(["sap/ui/core/ComponentSupport"]);
+        });
+    });
